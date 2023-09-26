967ee4af3c5f01839308c79050bfa9e56f4117e1
hessa <hessa93@mail.ru>
2023-09-26 22:13:05 +0300

    -

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL_SERVICE = "https://ec7f3516-82fe-4b9f-bed4-71676b9023ab.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders/"
+ORDER_TRACK = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Елена",
+    "lastName": "Драч",
+    "address": "Усачева, 3",
+    "metroStation": 34,
+    "phone": "89206227192",
+    "rentTime": 3,
+    "deliveryDate": "2023-09-27",
+    "comment": "Супер",
+    "color": [
+        "BLACK"
+    ]
+}
